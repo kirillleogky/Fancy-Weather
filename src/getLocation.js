9c132405631f40dc99acd000431a8eafeb075f66
Kirill Leogky <kirillmargheriti@gmail.com>
2020-05-02 11:14:23 +0300

    feat: improve adjust location by ip

--- src/getLocation.js
+++ src/getLocation.js
@@ -1,6 +1,17 @@
 export default async function getUserLocation() {
-  const token = 'd12f917a258822';
-  let response = await fetch(`https://ipinfo.io/json?token=${token}`);
+  const tokenIp = 'd12f917a258822';
+  let responseIp = await fetch(`https://ipinfo.io/json?token=${tokenIp}`);
+  responseIp = await responseIp.json();
+
+  const token = '25d5d7222a366b92e02ec8c9aee0902a';
+  let response = await fetch(`http://api.ipstack.com/${responseIp.ip}?access_key=${token}`);
   response = await response.json();
-  return response;
+
+  const lat = response.latitude.toFixed(4);
+  const long = response.longitude.toFixed(4);
+  return {
+    city: `${response.city}`,
+    loc: `${lat},${long}`,
+    country: `${response.country_code}`,
+  };
 }
