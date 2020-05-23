4f003ffb4d9b5e01e4d2a65e0e048dd23164a7bf
Kirill Leogky <kirillmargheriti@gmail.com>
2020-05-23 14:27:28 +0300

    refactor: reduce the number of code checked for errors

--- src/useGeocoding.js
+++ src/useGeocoding.js
@@ -14,83 +14,53 @@ function deletePreCountryInfo(elem) {
 export default async function getGeocoding(location) {
   let cityIp = await getUserLocation();
   cityIp = await cityIp.city;
+  let newCoord;
   try {
-    let newCoord = await fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&lang=en_US&apikey=${GEOCODE_MAPS_YANDEX_TOKEN}&geocode=${location}`);
+    newCoord = await fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&lang=en_US&apikey=${GEOCODE_MAPS_YANDEX_TOKEN}&geocode=${location}`);
     newCoord = await newCoord.json();
     newCoord = await newCoord.response;
     newCoord = await newCoord.GeoObjectCollection;
     newCoord = await newCoord.featureMember;
     newCoord = await newCoord[0];
     newCoord = await newCoord.GeoObject;
-    let coord = await newCoord.Point;
-    coord = await coord.pos;
-    coord = await coord.split(' ');
-    const [longitude, latitude] = await coord;
-    const country = await newCoord.description;
-    let countryCode = await newCoord.metaDataProperty;
-    countryCode = await countryCode.GeocoderMetaData;
-    countryCode = await countryCode.Address;
-    countryCode = await countryCode.country_code;
-
-    const city = await newCoord.name;
-
-    const currInfoObj = {
-      city: `${city}`,
-      loc: `${latitude},${longitude}`,
-      country: `${countryCode}`,
-    };
-
-    deletePreCountryInfo(document.getElementById('country'));
-    document.getElementById('country').prepend(`${await getCurrentCountry(country)}`);
-
-    document.getElementById('map').innerHTML = '';
-    let maps;
-    /* eslint-disable */
-        maps = new ymaps.Map('map', {
-          center: [0, 0],
-          zoom: 10
-        });
-        maps.setCenter([latitude, longitude]);
-    /* eslint-enable */
-    return currInfoObj;
   } catch (e) {
-    let newCoord = await fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&lang=en_US&apikey=${GEOCODE_MAPS_YANDEX_TOKEN}&geocode=${cityIp}`);
+    newCoord = await fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&lang=en_US&apikey=${GEOCODE_MAPS_YANDEX_TOKEN}&geocode=${cityIp}`);
     newCoord = await newCoord.json();
     newCoord = await newCoord.response;
     newCoord = await newCoord.GeoObjectCollection;
     newCoord = await newCoord.featureMember;
     newCoord = await newCoord[0];
     newCoord = await newCoord.GeoObject;
-    let coord = await newCoord.Point;
-    coord = await coord.pos;
-    coord = await coord.split(' ');
-    const [longitude, latitude] = await coord;
-    const country = await newCoord.description;
-    let countryCode = await newCoord.metaDataProperty;
-    countryCode = await countryCode.GeocoderMetaData;
-    countryCode = await countryCode.Address;
-    countryCode = await countryCode.country_code;
-
-    const city = await newCoord.name;
-
-    const currInfoObj = {
-      city: `${city}`,
-      loc: `${latitude},${longitude}`,
-      country: `${countryCode}`,
-    };
-
-    deletePreCountryInfo(document.getElementById('country'));
-    document.getElementById('country').prepend(`${await getCurrentCountry(country)}`);
-
-    document.getElementById('map').innerHTML = '';
-    let maps;
-    /* eslint-disable */
-        maps = new ymaps.Map('map', {
-          center: [0, 0],
-          zoom: 10
-        });
-        maps.setCenter([latitude, longitude]);
-    /* eslint-enable */
-    return currInfoObj;
   }
+  let coord = await newCoord.Point;
+  coord = await coord.pos;
+  coord = await coord.split(' ');
+  const [longitude, latitude] = await coord;
+  const country = await newCoord.description;
+  let countryCode = await newCoord.metaDataProperty;
+  countryCode = await countryCode.GeocoderMetaData;
+  countryCode = await countryCode.Address;
+  countryCode = await countryCode.country_code;
+
+  const city = await newCoord.name;
+
+  const currInfoObj = {
+    city: `${city}`,
+    loc: `${latitude},${longitude}`,
+    country: `${countryCode}`,
+  };
+
+  deletePreCountryInfo(document.getElementById('country'));
+  document.getElementById('country').prepend(`${await getCurrentCountry(country)}`);
+
+  document.getElementById('map').innerHTML = '';
+  let maps;
+  /* eslint-disable */
+      maps = new ymaps.Map('map', {
+        center: [0, 0],
+        zoom: 10
+      });
+      maps.setCenter([latitude, longitude]);
+  /* eslint-enable */
+  return currInfoObj;
 }
