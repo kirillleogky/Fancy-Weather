f2e9a6578dec7c41cee61bbf8494410b7e7995cd
Kirill Leogky <kirillmargheriti@gmail.com>
2020-06-21 19:14:45 +0300

    refactor: delete eslint-config-airbnb-base, add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,16 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+  },
+  extends: ["eslint:recommended"],
   rules: {
+    "no-console": 2,
+    "no-implied-eval": 2,
+    "no-empty-function": 2,
+    "no-constructor-return": 2,
     quotes: [2, "double", { avoidEscape: true, allowTemplateLiterals: true }],
   },
 };
